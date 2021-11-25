93df795092107d45c44547ac448647d0e525079a
inbruk <35333872+inbruk@users.noreply.github.com>
2021-11-25 22:43:31 +0300

    Добавьте файлы проекта.

---
+++ Publisher/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {            
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
