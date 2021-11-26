b9dce919e90efd067ca8e2c733ea19f99519ebe8
inbruk <35333872+inbruk@users.noreply.github.com>
2021-11-26 10:48:20 +0300

    .

--- Publisher/Program.cs
+++ Publisher/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using RabbitMQ.Client;
 
 namespace Publisher
@@ -10,8 +11,20 @@ namespace Publisher
     class Program
     {
         static void Main(String[] args)
-        {            
-            Console.WriteLine("Hello, World!");
+        {   
+            var factory = new ConnectionFactory() { HostName="localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("some-queue",false,false,false,null);
+
+                var message = "Some meassage for some queue";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "", routingKey: "some-queue", basicProperties:null, body:body);
+
+                Console.WriteLine("Message sent to default exchange !");
+            }
+            
             Console.ReadKey();
         }
     }
